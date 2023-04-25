00ed6b47c27bf4391040919564e03a6305677c12
Nurillo Mahmudjonov <72136850+iCloudmrx@users.noreply.github.com>
2023-04-25 18:17:24 +0500

    update gmail password

--- mysite/settings.py
+++ mysite/settings.py
@@ -169,7 +169,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 
 EMAIL_HOST_USER = 'newsfeed.uz@gmail.com'
 
-EMAIL_HOST_PASSWORD = 'jizxidvjongiivea'
+EMAIL_HOST_PASSWORD = 'ootpuzmainjkvrol'
 
 EMAIL_PORT = 587
 
