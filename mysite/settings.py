7e4720b8a6e1f34d63c6c5769fc7e8a13c21b4d5
Nurillo Mahmudjonov <72136850+iCloudmrx@users.noreply.github.com>
2023-04-25 16:05:01 +0500

    gmail pochtani olmashtirdim

--- mysite/settings.py
+++ mysite/settings.py
@@ -167,9 +167,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 
-EMAIL_HOST_USER = 'mahmudjonov2001uz@gmail.com'
+EMAIL_HOST_USER = 'newsfeed.uz@gmail.com'
 
-EMAIL_HOST_PASSWORD = 'jcarkxqudlxrkppb'
+EMAIL_HOST_PASSWORD = 'jizxidvjongiivea'
 
 EMAIL_PORT = 587
 
